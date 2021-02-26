97f72947cec79295386ea3a27ddb9e89542206c0
Yuan Zhou <yzhou@windsormachine.com>
2021-02-26 10:19:40 -0500

    author delete feature added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -89,18 +89,57 @@ exports.author_create_post = [
     }
 ];
 // Display Author delete on GET
-exports.author_delete_get = function(req, res){
-    res.send('NOT IMPLEMENTED: Author delete GET');
+exports.author_delete_get = function(req, res, next){
+    
+    async.parallel({
+        author: function(callback){
+            Author.findById(req.params.id).exec(callback);
+        },
+        authors_books: function(callback){
+            Book.find({'author':req.params.id}).exec(callback);
+        }
+    }, function(err, results){
+        if (err){return next(err);}
+        if (results.author==null){ // No results.
+            res.redirect('/catalog/authors');
+        }
+    
+        //successful, so render.
+        res.render('author_delete', {title:'Delete Author', author: results.author, author_books: results.authors_books});
+    });
 };
 
 // Handle Author delete on POST.
 exports.author_delete_post = function(req, res){
-    res.send('NOT IMPLEMENTED: Author delete POST');
+    async.parallel({
+        author: function(callback){
+            Author.findById(req.body.authorid).exec(callback);
+        },
+        authors_books: function(callback){
+            Book.find({'author':req.body.authorid}).exec(callback);
+        }
+    }, function(err, results){
+        if (err){return next(err);}
+        // Success
+        if (results.authors_books.length>0){
+            // Author has books. Render in same way as for GET route.
+            res.render('author_delete', {title: 'Delete Author', author:results.author, author_books:results.authors_books});
+            return;
+        }
+        Author.findByIdAndRemove(req.body.authorid, function deleteAutor(err){
+            if (err){return next(err);}
+            // Success - go to author list
+            res.redirect('/catalog/authors');
+            return ;
+        })
+
+    }
+    );
 };
 
 // Display Author update form on GET.
 exports.author_update_get = function(req,res){
-    res.send('NOT IMPLEMENTED: Author update GET');
+    res.render.send('NOT IMPLEMENTED: Author update get');
 };
 
 // Handle Author uddate on POST.
